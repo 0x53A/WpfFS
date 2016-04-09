c2fa8a5f8b8c67ff8cf9f565a8497c3942b220b6
lrieger <lr@live.de>
2016-04-10 01:56:03 +0200

    Add project files.

---
+++ FSLib/Library1.fs
@@ -0,0 +1,8 @@
+﻿namespace FSLib
+
+[<CLIMutableAttribute>]
+type MyRecord = { Name: string }
+
+[<CLIMutableAttribute>]
+type MyRecord2 = { A : MyRecord ; B : int }
+
---
+++ WpfFS/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<FSLib.MyRecord2> Items { get; } = new ObservableCollection<FSLib.MyRecord2>();
+
+        public MainWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+            //Items.Add(new FSLib.MyRecord2(new FSLib.MyRecord("x", n)))
+        }
+    }
+}
